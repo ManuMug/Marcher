9c30fc58ad3c652f22c6b9ec044b4ad1fb2a309d
ManuMug <manuelaporcel@hotmail.com>
2022-07-05 22:53:46 -0300

    creando vistas y controllers

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -16,6 +16,34 @@ const controllers ={
         })
           res.redirect("/")
       },
-}
+      editForm: (req, res) => {
+        db.Products.findByPk(req.params.id)
+      .then(products => {
+        res.render('products/productsEdit',{products})
+    
+      })
+    },
+    processEdit:(req, res)=>{
+        db.Products.update({
+        nombre: req.body.nombre,
+          precio: req.body.precio,
+          categorias: req.body.categorias,
+          descripcion: req.body.descripcion,
+          imagen: req.file.filename,
+        },{
+            where:{
+                idProduct:req.params.id
+            }
+        })
+        res.redirect('/')
+
+    },
+    detailForm:(req, res)=>{
+        db.Products.findByPk(req.params.id)
+        .then(products=>{
+            res.render('products/productsDetail',{products})
+        })
+    }
+    }
 
 module.exports = controllers
--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,12 @@ const controllers = require('../controllers/productsControllers')
 router.get('/',controllers.productsList)
 
 router.get('/create',controllers.createForm)
-
 router.post("/create", upload.single('imagen'), controllers.processCreate)
 
+router.get('/edit/:id',controllers.editForm)
+router.post('/edit/:id', upload.single('imagen'), controllers.processEdit)
+
+router.get('/detail/:id',controllers.detailForm)
+
 module.exports = router
+
