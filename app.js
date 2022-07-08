8e4ddc4e39761cb5741778c4d4aa130331b3b12c
ManuMug <manuelaporcel@hotmail.com>
2022-07-08 20:44:10 -0300

    error 404

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/',indexRouter)
 app.use('/users',usersRouter)
 app.use('/products',productsRouter)
 app.use(function (req, res, next) {
-    next(createError(404));
+    res.render('404');
   });
   
   // error handler
